915a3c92566aa9c627917f707c841bca7fe10860
dreamsxin <dreamsxin@126.com>
2018-01-04 08:49:49 +0800

    Change License to New BSD License

--- frontends/php/include/graphs.inc.php
+++ frontends/php/include/graphs.inc.php
@@ -1,22 +1,18 @@
 <?php
+
 /*
-** Allview
-** Copyright (C) 2000,2001,2002,2003,2004 Allview team 
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-**/
+  +------------------------------------------------------------------------+
+  | AllView                                                                |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2001-2008 Myleft Studio (http://www.myleftstudio.com)    |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License.                    |
+  +------------------------------------------------------------------------+
+  | Authors: Myleft Studio <dreamsxin@qq.com>                              |
+  |          Harald Holzer <hholzer@may.co.at>                             |
+  |          Dirk Datzert <dirk@datzert.de>                                |
+  +------------------------------------------------------------------------+
+*/
 ?>
 <?php
 	# Add Graph
